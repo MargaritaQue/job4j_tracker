60aff0776062111cb923b97f5b953aa373063c75
margarita <margarita.que@mail.ru>
2025-01-14 17:47:48 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -4,11 +4,9 @@ public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
         products[index] = null;
         for (int i = 0; i < products.length - 1; i++) {
-            if (products[i] == null) {
-                products[i] = products[i + 1];
-                products[i + 1] = null;
-            }
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
