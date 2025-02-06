0a9d17830dadffde6ee39ef1ffa22db1e95a2eb1
margarita <margarita.que@mail.ru>
2025-02-07 01:35:00 +0400

    0.4. Пользовательские исключения. [#219367 #531418]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,9 +3,10 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-            for (int i = 0; i < value.length - 1; i++) {
+            for (int i = 0; i < value.length; i++) {
                 if (value[i].equals(key)) {
                     result = i;
+                    break;
                 }
             }
         if (result == -1) {
