4471eda45992ba52ae3e44104eb8c8835e088b2c
margarita <margarita.que@mail.ru>
2025-02-07 13:10:40 +0400

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.MockInput;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenNotInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenNotInvalidInput2() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1", "2"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
+    }
+
+    @Test
+    void whenNotInvalidInputMinus() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+}
