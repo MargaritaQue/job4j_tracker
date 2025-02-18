d4e3cea2b04d495369170c9256bb3a12bf78dea1
margarita <margarita.que@mail.ru>
2025-02-18 09:53:57 +0400

    0. Map, HashMap [#212652 #532911]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("margarita.que@mail.ru", "Margarita Korol");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("margarita.que@mail.ru", "Margarita Fedorchuk");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
