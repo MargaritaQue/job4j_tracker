572b171c4e3e7fecd66cb4ac72fc69d56a7ec93f
margarita <margarita.que@mail.ru>
2025-03-03 18:52:15 +0400

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -3,11 +3,7 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
-//- метод split() класса String - необходим для того, чтобы отделить часть строки, которая будет содержать номер задачи;
-//
-//- метод Integer.parseInt() - позволит преобразовать строку, содержащее номер задачи в число типа Integer;
-//
-//- метод Integer.compare() - позволит корректно сравнить номера задач.
+
     @Override
     public int compare(String left, String right) {
         String[] l = left.split("\\.");
