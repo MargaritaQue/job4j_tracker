7baf4b1a219a9dd23faf508ff98d11ec91874fb8
margarita <margarita.que@mail.ru>
2025-03-12 04:26:57 +0400

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -61,7 +61,7 @@ public class AnalyzeByMap {
         Map<String, Integer> sub = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                sub.put(subject.name(), subject.score() + sub.getOrDefault(subject.name(), 0));
+                sub.merge(subject.name(), subject.score(), Integer::sum);
             }
         }
         LinkedList<Label> labels = new LinkedList<>();
