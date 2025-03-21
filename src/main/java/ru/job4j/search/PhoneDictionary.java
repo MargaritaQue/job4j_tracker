25d116746df0300c3830a170e360b5b05a157303
margarita <margarita.que@mail.ru>
2025-03-21 22:35:14 +0400

    2. Local-Variable Type Inference [#70626]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,13 +11,13 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
+        var result = new ArrayList<Person>();
         Predicate<Person> predicateName = (f) -> f.getName().contains(key);
         Predicate<Person> predicateSurname = (f) -> f.getSurname().contains(key);
         Predicate<Person> predicatePhone = (f) -> f.getPhone().contains(key);
         Predicate<Person> predicateAddress = (f) -> f.getAddress().contains(key);
         Predicate<Person> predicateAll = predicateName.or(predicateSurname).or(predicatePhone).or(predicateAddress);
-        for (Person person : persons) {
+        for (var person : persons) {
               if (predicateAll.test(person)) {
                   result.add(person);
               }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,31 +1,29 @@
 package ru.job4j.search;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 
     @Test
     public void whenNoFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Ivan");
-        assertTrue(persons.isEmpty());
+        var persons = phones.find("Ivan");
+        Assertions.assertTrue(persons.isEmpty());
     }
 
 }
